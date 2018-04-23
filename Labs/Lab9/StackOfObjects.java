677cc4d8e67c9390b89d38dec02431cdd72b1bb2
Ivan <tinovivan96@gmail.com>
2018-04-22 23:01:33 -0400

    Update StackOfObjects.java

--- Labs/Lab9/StackOfObjects.java
+++ Labs/Lab9/StackOfObjects.java
@@ -1,9 +1,5 @@
 package Labs.Lab9;
-/*  CSE 114 Summer 2017, Paul Fodor
- *  Ivan Tinov
- *  ID# 110255332
- *  Lab #9
- *  
+/*  
  *  This lab actually is a simple implementation of a Stack, which is further
  *  used and taught in CSE 214
  */
